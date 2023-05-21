656e33291d7017f3c5bbaf48281b72b5eaebc438
saurabh2580 <126052537+saurabh2580@users.noreply.github.com>
2023-05-21 09:36:33 +0530

    Update CategoryRepo.java

--- CategoryRepo.java
+++ CategoryRepo.java
@@ -1,9 +1,11 @@
 package com.api.nimapinfotechTest.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.api.nimapinfotechTest.model.Category;
 
-public interface CategoryRepo extends JpaRepository<Category, Integer> {
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 }
