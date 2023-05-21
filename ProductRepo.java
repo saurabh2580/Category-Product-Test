0c0e8ace1c1c4ee518c2b2c7bf83dbbc9126223d
saurabh2580 <126052537+saurabh2580@users.noreply.github.com>
2023-05-21 09:37:59 +0530

    Update ProductRepo.java

--- ProductRepo.java
+++ ProductRepo.java
@@ -1,12 +1,9 @@
 package com.api.nimapinfotechTest.repository;
 
-
+import com.api.nimapinfotechTest.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.api.nimapinfotechTest.model.Product;
-
 @Repository
-public interface ProductRepo extends JpaRepository<Product, Integer> {
-
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
